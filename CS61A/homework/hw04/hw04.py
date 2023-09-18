512f5465725edaeec7a1b81b9865f202254b873a
ddrEricNo1 <ericDing1811@outlook.com>
2023-09-19 00:31:14 +0800

    9/19

--- CS61A/homework/hw04/hw04.py
+++ CS61A/homework/hw04/hw04.py
@@ -52,13 +52,13 @@ def planet(size):
     """Construct a planet of some size."""
     assert size > 0
     "*** YOUR CODE HERE ***"
-
+    return ['planet', size]
 
 def size(w):
     """Select the size of a planet."""
     assert is_planet(w), 'must call size on a planet'
     "*** YOUR CODE HERE ***"
-
+    return w[1]
 
 def is_planet(w):
     """Whether w is a planet."""
@@ -118,7 +118,12 @@ def balanced(m):
     True
     """
     "*** YOUR CODE HERE ***"
-
+    if is_planet(m):
+        return True
+    left_arm, right_arm = left(m), right(m)
+    left_thing, right_thing = end(left_arm), end(right_arm)    
+    overall_balance = total_weight(left_thing) * length(left_arm) == total_weight(right_thing) * length(right_arm)
+    return balanced(left_thing) and balanced(right_thing) and overall_balance 
 
 def totals_tree(m):
     """Return a tree representing the mobile with its total weight at the root.
@@ -150,6 +155,11 @@ def totals_tree(m):
     True
     """
     "*** YOUR CODE HERE ***"
+    # base case
+    if is_planet(m):
+        return tree(size(m))
+    else:
+        return tree(total_weight(m), [totals_tree(end(left(m))), totals_tree(end(right(m)))])
 
 
 def replace_thor_at_leaf(t, thors_replacement):
@@ -182,7 +192,10 @@ def replace_thor_at_leaf(t, thors_replacement):
     True
     """
     "*** YOUR CODE HERE ***"
-
+    if is_leaf(t) and label(t) == "thor":
+        return tree(thors_replacement)
+    else:
+        return tree(label(t), [replace_thor_at_leaf(b, thors_replacement) for b in branches(t)])
 
 def has_path(t, word):
     """Return whether there is a path in a tree where the entries along the path
@@ -216,7 +229,15 @@ def has_path(t, word):
     """
     assert len(word) > 0, 'no path for empty word.'
     "*** YOUR CODE HERE ***"
-
+    if len(word) == 1:
+        return word == label(t)
+    
+    # 递归调用过程：word中最前的字符是匹配的，并且之后也有路径能够匹配到剩余的字符
+    if label(t) == word[0]:
+        for b in branches(t):
+            if has_path(b, word[1: ]):
+                return True
+    return False
 
 def preorder(t):
     """Return a list of the entries in this tree in the order that they
@@ -229,7 +250,12 @@ def preorder(t):
     [2, 4, 6]
     """
     "*** YOUR CODE HERE ***"
-
+    if is_leaf(t):
+        return [label(t)]
+    result = [label(t)]
+    for b in branches(t):
+        result.extend(preorder(b))
+    return result
 
 def interval(a, b):
     """Construct an interval from a to b."""
@@ -239,12 +265,12 @@ def interval(a, b):
 def lower_bound(x):
     """Return the lower bound of interval x."""
     "*** YOUR CODE HERE ***"
-
+    return x[0]
 
 def upper_bound(x):
     """Return the upper bound of interval x."""
     "*** YOUR CODE HERE ***"
-
+    return x[1]
 
 def str_interval(x):
     """Return a string representation of interval x.
@@ -263,24 +289,25 @@ def add_interval(x, y):
 def mul_interval(x, y):
     """Return the interval that contains the product of any value in x and any
     value in y."""
-    p1 = x[0] * y[0]
-    p2 = x[0] * y[1]
-    p3 = x[1] * y[0]
-    p4 = x[1] * y[1]
-    return [min(p1, p2, p3, p4), max(p1, p2, p3, p4)]
+    p1 = lower_bound(x) * lower_bound(y)
+    p2 = lower_bound(x) * upper_bound(y)
+    p3 = upper_bound(x) * lower_bound(y)
+    p4 = upper_bound(x) * upper_bound(y)
+    return interval(min(p1, p2, p3, p4), max(p1, p2, p3, p4))
 
 
 def sub_interval(x, y):
     """Return the interval that contains the difference between any value in x
     and any value in y."""
     "*** YOUR CODE HERE ***"
-
+    return interval(lower_bound(x) - upper_bound(y), upper_bound(x) - lower_bound(y))
 
 def div_interval(x, y):
     """Return the interval that contains the quotient of any value in x divided by
     any value in y. Division is implemented as the multiplication of x by the
     reciprocal of y."""
     "*** YOUR CODE HERE ***"
+    assert lower_bound(y) * upper_bound(y) >= 0
     reciprocal_y = interval(1 / upper_bound(y), 1 / lower_bound(y))
     return mul_interval(x, reciprocal_y)
 
@@ -305,8 +332,8 @@ def check_par():
     >>> lower_bound(x) != lower_bound(y) or upper_bound(x) != upper_bound(y)
     True
     """
-    r1 = interval(1, 1)  # Replace this line!
-    r2 = interval(1, 1)  # Replace this line!
+    r1 = interval(1, 100000)  # Replace this line!
+    r2 = interval(1, 100000)  # Replace this line!
     return r1, r2
 
 
@@ -324,8 +351,20 @@ def quadratic(x, a, b, c):
     '0 to 10'
     """
     "*** YOUR CODE HERE ***"
-
-
+    f = lambda x: a * (x ** 2) + b * x + c 
+    s_axis = -b / (2 * a)
+    x1, x2 = lower_bound(x), upper_bound(x)
+    y1, y2 = f(x1), f(x2)
+    value = f(s_axis)
+    
+    if x1 <= s_axis <= x2:
+        if a > 0:
+            return interval(value, max(y1, y2))
+        else:
+            return interval(min(y1, y2), value)
+    else:
+        return interval(min(y1, y2), max(y1, y2))
+    
 # Tree ADT
 
 def tree(label, branches=[]):
