390266a78a4d35f6f15dd75da7cd8cec35026804
ddrEricNo1 <ericDing1811@outlook.com>
2023-09-28 17:48:49 +0800

    9/28

--- CS61A/homework/hw06/hw06.scm
+++ CS61A/homework/hw06/hw06.scm
@@ -1,11 +1,35 @@
 (define (cddr s) (cdr (cdr s)))
 
-(define (cadr s) 'YOUR-CODE-HERE)
+(define (cadr s) (car (cdr s)))
 
-(define (caddr s) 'YOUR-CODE-HERE)
+(define (caddr s) (car (cdr (cdr s))))
 
-(define (sign val) 'YOUR-CODE-HERE)
+(define (sign val) (cond ((< val 0) -1)
+                        ((= val 0) 0)
+                        (#t 1)))
 
 (define (square x) (* x x))
 
-(define (pow base exp) 'YOUR-CODE-HERE)
+(define (pow base exp) 
+    (
+        if (= exp 1) base
+        (if (even? exp) 
+            (
+                square (pow base (/ exp 2))
+            )
+            (
+                * base (square (pow base (floor (/ exp 2))))
+            )
+        )
+    )
+)
+
+(define (pow x y) 
+    'YOUR-CODE-HERE
+    (if (= y 1) x
+        (if (even? y) 
+            (square (pow x (/ y 2)))
+            (* x (square (pow x (floor (/ y 2)))))
+        )
+    )
+)
