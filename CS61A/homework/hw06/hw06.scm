67b4c406e43d4b5e4ce8a5d8c69f2205c291c31a
ddrEricNo1 <ericDing1811@outlook.com>
2023-09-29 02:48:39 +0800

    9/29

--- CS61A/homework/hw06/hw06.scm
+++ CS61A/homework/hw06/hw06.scm
@@ -24,12 +24,3 @@
     )
 )
 
-(define (pow x y) 
-    'YOUR-CODE-HERE
-    (if (= y 1) x
-        (if (even? y) 
-            (square (pow x (/ y 2)))
-            (* x (square (pow x (floor (/ y 2)))))
-        )
-    )
-)
