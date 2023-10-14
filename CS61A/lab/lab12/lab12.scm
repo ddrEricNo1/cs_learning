c939fbe72a6867f23535a09378586c58a30b6a93
ddrEricNo1 <ericDing1811@outlook.com>
2023-10-15 01:42:25 +0800

    interesting lab12

--- CS61A/lab/lab12/lab12.scm
+++ CS61A/lab/lab12/lab12.scm
@@ -1,10 +1,28 @@
-(define (tail-replicate x n) 'YOUR-CODE-HERE)
+(define (tail-replicate x n)
+  'YOUR-CODE-HERE
+  (define (helper cur i)
+    (if (= i n)
+        `(,@cur ,x)
+        (helper `(,@cur ,x) (+ i 1))
+    )
+  )
+  (if (= n 0)
+      '()
+      (helper '() 1)
+  )
+)
 
 (define-macro (def func args body)
-  'YOUR-CODE-HERE)
+  'YOUR-CODE-HERE
+  `(define (,func ,@args) ,body)
+)
 
 (define (repeatedly-cube n x)
   (if (zero? n)
       x
-      (let (_________________________)
-        (* y y y))))
+      (let ((y (repeatedly-cube (- n 1) x))
+           )
+        (* y y y)
+      )
+  )
+)
