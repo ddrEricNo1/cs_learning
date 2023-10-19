bcb3bd4e756527756be302e012b55fbe0ccd8b56
ddrEricNo1 <ericDing1811@outlook.com>
2023-10-19 13:00:21 +0800

    10/19

--- CS106L/lecture/lecture1_streams/1.cpp
+++ CS106L/lecture/lecture1_streams/1.cpp
@@ -5,10 +5,21 @@ using namespace std;
 
 int main() {
 	// output string stream
+	// coonstructors with initial text in the buffer. can optionally provide modes such as ate(start at end) or bin(read as binary)
 	ostringstream oss("Ito En Green Tea");	// construct oss with the string parameter as the initial string
 	cout << oss.str() << endl;	// take whatever in the stream array and output it	
 
-	oss << 16.9 << "Ounce";
+	// one thing to notice is that, the type of 16.9 is double, sringsream takes whaatever type you give to it and automatically 
+	// converts that type to the string represenation of that type.
+	oss << 16.9 << "Ounce ";	// when we open a stringstream, it opens it at the beginning, so it will overwrite what in the initial stream
+	cout << oss.str() << endl;
+
+	// an option is to add one attribute stringstream:ate, which means open a stream at the end
+	ostringstream oss1("Ito En Green Tea", ostringstream::ate);
+	oss1 << "test";
+	cout << oss1.str() << endl;	// str() method takes everthing into the buffer and turns it into a string
+
+	oss << "(Pack of " << 12 << ")";
 	cout << oss.str() << endl;
 	return 0;
 }
