ac4429cb08212f8997316594467985fe45e283ed
ddrEricNo1 <ericDing1811@outlook.com>
2023-12-06 01:54:50 +0800

    12/6

--- CS106L/lecture/lecture2_streams/test.cpp
+++ CS106L/lecture/lecture2_streams/test.cpp
@@ -1,14 +1,10 @@
 #include <sstream>
 #include <iostream>
-using namespace std;
 
 int main() {
-	string name;
-	string age;
-	cout << "what is your name?";
-	cin >> name;
-	cout << "what is your age?";
-	cin >>age;
-	cout << "hello " << name << " your age is: " << age << endl;
+	std::cout << "cs";
+	std::cout << 106;
+	std::cout << std::flush;
+	std::cout << 'L' << std::endl;
 	return 0;
 }
